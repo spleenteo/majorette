8fc9954f4716d8c6b651667341eadfe501db2da9
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2014-12-20 19:58:27 +0100

    migration users for lists and items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :list
+  belongs_to :user
   validates :title, presence: true
 
   default_scope { order("created_at desc") }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141220142213) do
+ActiveRecord::Schema.define(version: 20141220185642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20141220142213) do
     t.integer  "list_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "lists", force: true do |t|
