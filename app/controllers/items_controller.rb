aa3c2f770e7896e2e7001d0d781e1f566a5a4fe0
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2014-12-20 11:55:00 +0100

    add items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,32 @@
 class ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+
+    if @item.save
+      redirect_to list_path(@item.list_id)
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :url, :list_id)
+  end
+
 end
