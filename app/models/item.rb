4e39014e2bd467eb781f55ab41e60eaa9ea4021c
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2014-12-21 22:11:36 +0100

    wip useless

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ActiveRecord::Base
   belongs_to :list
   belongs_to :user
   validates :title, presence: true
-  default_scope { order("created_at desc") }
-  scope :latest, -> { limit(5) }
 
+  default_scope { order(updated_at: :desc) }
+  scope :latest, -> { order("created_at desc").limit(5) }
 end
