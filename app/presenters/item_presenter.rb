d8adca9816d65757ff430fa11fbcc7867d764969
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2014-12-21 23:59:41 +0100

    set presenter for item with minimum style

---
+++ app/presenters/item_presenter.rb
@@ -0,0 +1,25 @@
+class ItemPresenter < Showcase::Presenter
+
+  def full_title
+    if url.present?
+      h.link_to title, url, target: "_blank"
+    else
+      title
+    end
+  end
+
+  def author
+    if user.present? && user.id == h.current_user.id
+      u = "you"
+    else
+      u = user.email.split("@").first if user.present?
+    end
+    "by #{u}"
+  end
+
+  def meta
+    "#{h.time_ago_in_words(created_at)} #{author}"
+  end
+
+end
+
